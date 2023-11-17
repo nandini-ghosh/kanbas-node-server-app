1f3dd7c66d6600925789ff44f5266987ac62ddee
nandini-ghosh <nandini.ghosh2094@gmail.com>
2023-11-17 15:37:51 -0500

    changes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
+app.use(cors());
 Lab5(app);
-Hello(app);
-app.listen(4000)
+app.listen(4000);
 
 
